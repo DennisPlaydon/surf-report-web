9df07048bc162423d37425601e5c03c89dfa9e55
Dennis Playdon <dennis.playdon@xero.com>
2021-11-01 20:05:47 +1300

    Add initial surf lines

---
+++ pages/api/SingleForecast.ts
@@ -0,0 +1,6 @@
+export class SingleForecast {
+    date: string | undefined;
+    tenAmFaceHeight: string | undefined;
+    fourPmFaceHeight: string | undefined;
+    tenPmFaceHeight: string | undefined;
+}
