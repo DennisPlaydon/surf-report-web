643b47418d163b9c044e7fb58b7503101192ab44
Dennis Playdon <dennis.playdon@xero.com>
2021-11-01 23:20:41 +1300

    Move metservice data fetch directly inline with getserverside props

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,13 +55,6 @@ const Home: NextPage = ({ surfData }: any) => {
                             dataKey="Waihi"
                             stroke="#3705AD"
                         />
-                        {/* {beaches.map((beach) => {
-                            <Line
-                                type="monotone"
-                                dataKey={beach}
-                                stroke="#00f590"
-                            />;
-                        })} */}
                     </LineChart>
                 </ResponsiveContainer>
             </main>
@@ -84,10 +77,58 @@ export async function getServerSideProps() {
     const res = await fetch(baseUrl + "/api/metservice");
     const surfData = await res.json();
 
+    const faceHeightIndex = 5;
+    const surfDateTimeIndex = 0;
+
+    const beaches = [
+        { name: "Piha", region: "piha", location: "piha" },
+        { name: "Omaha", region: "great-barrier", location: "omaha" },
+        { name: "Muriwai", region: "piha", location: "muriwai-beach" },
+        {
+            name: "PortWaikato",
+            region: "west-auckland",
+            location: "port-waikato",
+        },
+        { name: "Waihi", region: "coromandel", location: "waihi-beach" },
+    ];
+
+    var combinedForecast: { [time: string]: Array<Object> } = {};
+    for (let beachIndex = 0; beachIndex < beaches.length; beachIndex++) {
+        const beach = beaches[beachIndex];
+
+        const result = await fetch(
+            `https://www.metservice.com/publicData/webdata/marine/regions/${beach.region}/surf/locations/${beach.location}`
+        );
+        const json = await result.json();
+        const daysForecast: Array<String> =
+            json["layout"]["primary"]["slots"]["main"]["modules"][0]["days"];
+
+        daysForecast.forEach((individualDay: any) => {
+            const surfTimes = individualDay["rows"][surfDateTimeIndex]["data"];
+            const waveFaceHeights =
+                individualDay["rows"][faceHeightIndex]["data"];
+
+            for (let index = 0; index < surfTimes.length; index++) {
+                const beachName: string = beach.name;
+                let setFaceEntry: any = {};
+                setFaceEntry[beachName] = waveFaceHeights[index]["setFace"];
+
+                var newArray = combinedForecast[surfTimes[index]["at"]];
+                newArray
+                    ? newArray.push(setFaceEntry)
+                    : (newArray = [setFaceEntry]);
+                combinedForecast[surfTimes[index]["at"]] = newArray;
+            }
+        });
+    }
+
+    const formattedData = Object.entries(combinedForecast).map(
+        ([key, values]) => ({ time: key, ...Object.assign({}, ...values) })
+    );
+
     return {
         props: {
-            surfData,
-            baseUrl,
+            surfData: formattedData,
         },
     };
 }
