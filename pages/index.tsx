7879b5c031ec95715cec85400f69d8bed934574a
Dennis Playdon <dennis.playdon@xero.com>
2021-11-06 14:20:21 +1300

    Add period data

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,52 +2,35 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import React, { useState } from "react";
 import { ButtonGroup, Button } from "react-bootstrap";
+import { getIndexPageProps } from "../app/propsGenerators/indexPagePropsGenerator";
+import { IndexPageProps } from "../app/types/IndexPageProps";
 import styles from "../styles/Home.module.css";
 
-const generateRanHex = () =>
-    "#" +
-    [...Array(6)]
-        .map(() => Math.floor(Math.random() * 16).toString(16))
-        .join("");
-
-const Home: NextPage = ({ surfData, windData }: any) => {
+const Home: NextPage<IndexPageProps> = ({
+    surfData,
+    windData,
+    periodData,
+}: IndexPageProps) => {
     const beaches = Object.keys(surfData[0]).filter((x) => x !== "time");
 
-    const getSurfDataForDate = (date: Date) =>
-        surfData.filter(
-            (x: any) => new Date(x.time).getDate() === date.getDate()
-        );
-
-    const getWindDataForDate = (date: Date) =>
-        windData.filter(
-            (x: any) => new Date(x.time).getDate() === date.getDate()
-        );
+    const filterDataForDate = (date: Date, data: any) =>
+        data.filter((x: any) => new Date(x.time).getDate() === date.getDate());
 
     const [dailySurfData, setDailySurfData] = useState(
-        getSurfDataForDate(new Date())
+        filterDataForDate(new Date(), surfData)
     );
     const [dailyWindData, setDailyWindData] = useState(
-        getWindDataForDate(new Date())
+        filterDataForDate(new Date(), windData)
+    );
+    const [dailyPeriodData, setDailyPeriodData] = useState(
+        filterDataForDate(new Date(), periodData)
     );
 
-    const axisFormatter = (value: string) =>
-        new Date(value).toLocaleString("en-En", {
-            month: "short",
-            day: "numeric",
-            hour: "numeric",
-        });
-
-    const getAverageSurfValue = (x: string) =>
-        dailySurfData.reduce(
-            (prev: number, cur: any) => prev + Number(cur[x]),
+    const average = (beachName: string, array: any) =>
+        array.reduce(
+            (prev: number, cur: any) => prev + Number(cur[beachName]),
             0
-        ) / dailySurfData.length;
-
-    const getAverageWindValue = (x: string) =>
-        dailyWindData.reduce(
-            (prev: number, cur: any) => prev + Number(cur[x]),
-            0
-        ) / dailyWindData.length;
+        ) / array.length;
 
     return (
         <div className={styles.container}>
@@ -63,10 +46,18 @@ const Home: NextPage = ({ surfData, windData }: any) => {
                     {beaches.map((x) => (
                         <div className={styles.card} key={x}>
                             <p>{x}</p>
-                            <h2>{getAverageSurfValue(x).toFixed(1)}m</h2>
+                            <h2>{average(x, dailySurfData).toFixed(1)}m</h2>
                             <p>
-                                <i className="bi bi-wind"></i>{" "}
-                                {getAverageWindValue(x).toFixed(1)} kts
+                                <i className="bi bi-wind"></i> Wind -{" "}
+                                {average(x, dailyWindData).toFixed(1)}
+                                kts
+                            </p>
+                            <p>
+                                <i className="bi bi-tsunami"></i> Period -{" "}
+                                {Math.round(
+                                    Number(average(x, dailyPeriodData))
+                                )}
+                                s
                             </p>
                         </div>
                     ))}
@@ -78,8 +69,15 @@ const Home: NextPage = ({ surfData, windData }: any) => {
                             variant="outline-secondary"
                             onClick={() => {
                                 const today = new Date();
-                                setDailySurfData(getSurfDataForDate(today));
-                                setDailyWindData(getWindDataForDate(today));
+                                setDailySurfData(
+                                    filterDataForDate(today, surfData)
+                                );
+                                setDailyWindData(
+                                    filterDataForDate(today, windData)
+                                );
+                                setDailyPeriodData(
+                                    filterDataForDate(today, periodData)
+                                );
                             }}
                         >
                             Today
@@ -89,8 +87,15 @@ const Home: NextPage = ({ surfData, windData }: any) => {
                             onClick={() => {
                                 var tomorrow = new Date();
                                 tomorrow.setDate(tomorrow.getDate() + 1);
-                                setDailySurfData(getSurfDataForDate(tomorrow));
-                                setDailyWindData(getWindDataForDate(tomorrow));
+                                setDailySurfData(
+                                    filterDataForDate(tomorrow, surfData)
+                                );
+                                setDailyWindData(
+                                    filterDataForDate(tomorrow, windData)
+                                );
+                                setDailyPeriodData(
+                                    filterDataForDate(tomorrow, periodData)
+                                );
                             }}
                         >
                             Tomorrow
@@ -113,89 +118,8 @@ const Home: NextPage = ({ surfData, windData }: any) => {
 };
 
 export async function getServerSideProps() {
-    const surfDateTimeIndex = 0;
-    const windLevelIndex = 2;
-    const faceHeightIndex = 5;
-
-    const beaches = [
-        { name: "Piha", region: "piha", location: "piha" },
-        { name: "Omaha", region: "great-barrier", location: "omaha" },
-        { name: "Muriwai", region: "piha", location: "muriwai-beach" },
-        {
-            name: "PortWaikato",
-            region: "west-auckland",
-            location: "port-waikato",
-        },
-        { name: "Waihi", region: "coromandel", location: "waihi-beach" },
-    ];
-
-    var surfData: { [time: string]: Array<Object> } = {};
-    var windData: { [time: string]: Array<Object> } = {};
-    for (let beachIndex = 0; beachIndex < beaches.length; beachIndex++) {
-        const beach = beaches[beachIndex];
-
-        const result = await fetch(
-            `https://www.metservice.com/publicData/webdata/marine/regions/${beach.region}/surf/locations/${beach.location}`
-        );
-        const json = await result.json();
-        const daysForecast: Array<String> =
-            json["layout"]["primary"]["slots"]["main"]["modules"][0]["days"];
-
-        daysForecast.forEach((individualDay: any) => {
-            const individualDayForecastTimes =
-                individualDay["rows"][surfDateTimeIndex]["data"];
-            const waveFaceHeights =
-                individualDay["rows"][faceHeightIndex]["data"];
-            const windLevels = individualDay["rows"][windLevelIndex]["data"];
-
-            const startingIndexExcluding4AmTime = 1;
-            for (
-                let index = startingIndexExcluding4AmTime;
-                index < individualDayForecastTimes.length;
-                index++
-            ) {
-                let time = individualDayForecastTimes[index]["at"];
-
-                let setFaceEntry: any = {};
-                setFaceEntry[beach.name] = waveFaceHeights[index]["setFace"];
-
-                var surfEntriesForDate = surfData[time];
-                surfEntriesForDate
-                    ? surfEntriesForDate.push(setFaceEntry)
-                    : (surfEntriesForDate = [setFaceEntry]);
-                surfData[time] = surfEntriesForDate;
-
-                let windEntry: any = {};
-                windEntry[beach.name] = windLevels[index]["gust"];
-
-                var windEntriesForDate = windData[time];
-                windEntriesForDate
-                    ? windEntriesForDate.push(windEntry)
-                    : (windEntriesForDate = [windEntry]);
-                windData[time] = windEntriesForDate;
-            }
-        });
-    }
-
-    const formattedSurfData = Object.entries(surfData).map(
-        ([key, values]: [string, any]) => ({
-            time: key,
-            ...Object.assign({}, ...values),
-        })
-    );
-
-    const formattedWindData = Object.entries(windData).map(
-        ([key, values]: [string, any]) => ({
-            time: key,
-            ...Object.assign({}, ...values),
-        })
-    );
-
     return {
-        props: {
-            surfData: formattedSurfData,
-            windData: formattedWindData,
-        },
+        props: await getIndexPageProps(),
     };
 }
 
