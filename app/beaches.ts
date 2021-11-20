262203aa0b7a7f9aa567312a727a67b02b2e1224
Dennis Playdon <dennis.playdon@xero.com>
2021-11-20 22:01:52 +1300

    Add crude sorting to beaches

--- app/beaches.ts
+++ app/beaches.ts
@@ -1,13 +1,20 @@
 import { Beach } from "./types/Beach";
 
-let piha: Beach = { name: "Piha", region: "piha", location: "piha" };
+// Auckland east coast
 let omaha: Beach = { name: "Omaha", region: "great-barrier", location: "omaha" };
+let tawharanui: Beach = { name: "Tawharanui", region: "great-barrier", location: "tawharanui" };
+let longBay: Beach = { name: "Long Bay", region: "east-auckland", location: "long-bay" };
+
+// Auckland west coast
+let piha: Beach = { name: "Piha", region: "piha", location: "piha" };
 let muriwai: Beach = { name: "Muriwai", region: "piha", location: "muriwai-beach" };
 let portWaikato: Beach = {
     name: "Port Waikato",
     region: "west-auckland",
     location: "port-waikato",
 };
+
+// Bay of plenty
 let waihi: Beach = { name: "Waihi", region: "coromandel", location: "waihi-beach" };
 
-export const beaches = [piha, omaha, muriwai, portWaikato, waihi];
+export const beaches = [piha, omaha, longBay, tawharanui, muriwai, portWaikato, waihi];
