2a01e83af068f02181cf0dc1f4cf4ee484a73b50
Dennis Playdon <37030744+DennisPlaydon@users.noreply.github.com>
2022-02-14 17:04:05 +1300

    Update readme (#36)
    Co-authored-by: Dennis Playdon <dennis.playdon@xero.com>

---
+++ app/helpers/isToday.ts
@@ -0,0 +1,10 @@
+const isToday = (date: Date) => {
+    var today = new Date();
+    return (
+        date.getDate() === today.getDate() &&
+        date.getMonth() === today.getMonth() &&
+        date.getFullYear() === today.getFullYear()
+    );
+};
+
+export default isToday;
